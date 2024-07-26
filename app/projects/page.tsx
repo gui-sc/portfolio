aae0e9eacf877cbfc7b4e8d0dfe3eae270a7a90a
guilherme coelho <guilhermeifc7@gmail.com>
2024-07-26 20:36:05 -0300

    feat: add new tech skills and wip project

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -21,6 +21,20 @@ export default function Page() {
                 repository: "https://github.com/gui-sc/portfolio",
                 languages: ["TypeScript", "React", "TailwindCSS"]
             },
+            {
+                name: "Salon App (WIP)",
+                description: {
+                    pt: `Este é um projeto em andamento, onde estou desenvolvendo um web app para salões de beleza e barbearias. 
+                    O objetivo desse projeto é ser um projeto white label, em que qualquer cliente possa customizar o app de acordo
+                    com a sua logo, cores e serviços. O projeto tem uma stack completa, com um back-end em NodeJs, um front-end em React
+                    e banco de dados em MongoDB.`,
+                    en: `This is a work in progress project, where I'm developing a web app for beauty salons and barbershops.
+                    This project's goal is to be a white label app, where any client can customize the app with their logo, colors palette
+                    and services. The project has a full stack, with a NodeJs back-end, a React front-end and a MongoDB database.`
+                },
+                repository: "htpps://github.com/gui-sc/salonApp-api",
+                languages: ["TypeScript", "React", "NodeJs", "MongoDB"]
+            }
         ])
     }
 
