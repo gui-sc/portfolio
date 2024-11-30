b918fba078bda2fcd40b24ff9f7b2eb125ac0264
guilherme coelho <guilhermeifc7@gmail.com>
2024-11-30 10:03:02 -0300

    feat: adicionar novos projetos ao portfólio e atualizar descrições

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -23,6 +23,23 @@ export default function Page() {
                 languages: ["TypeScript", "NextJs", "TailwindCSS"]
             },
             {
+                name: "Salon App API (WIP)",
+                description: {
+                    pt: `Este é um projeto em andamento, onde estou desenvolvendo um web app para salões de beleza e barbearias. 
+                    O objetivo desse projeto é ser um projeto white label, em que qualquer cliente possa customizar o app de acordo
+                    com a sua logo, cores e serviços. O projeto tem uma stack completa, com um back-end em NodeJs, um front-end em React
+                    e banco de dados em MongoDB. Atualmente finalizei a parte de funções básicas do back-end, para inserção, atualização, 
+                    leitura e remoção dos dados, irei agora seguir com a implementação de autenticação e autorização.`,
+                    en: `This is a work in progress project, where I'm developing a web app for beauty salons and barbershops.
+                    This project's goal is to be a white label app, where any client can customize the app with their logo, colors palette
+                    and services. The project has a full stack, with a NodeJs back-end, a React front-end and a MongoDB database.
+                    I've finished the basic functions of the back-end, for CRUD operations, and now I'll move on to the implementation
+                    of authentication and authorization.`
+                },
+                repository: `${GITHUB_URL}/salonApp-api`,
+                languages: ["TypeScript", "Express", "NodeJs", "MongoDB"]
+            },
+            {
                 name: "Rosacleo",
                 description: {
                     en: `This is a Mocked E-commerce Web App, built with Next.js, Typescript and Tailwind CSS. This Web App was made for a friend's high school project. The main goal of this project was to create a simple and responsive e-commerce website, with a focus on the user experience. The website has a product page, a cart page, and a checkout page, as well as a discount coupon.`,
@@ -63,6 +80,21 @@ export default function Page() {
                 languages: ["TypeScript", "React Native", "Google Maps API"]
             },
             {
+                name: 'Page Flow',
+                description: {
+                    en: `This project is a Pomodoro timer with a kindle-like design. The main goal is to help people to 
+                    focus on their books and studies. The project was made with HTML, CSS and JavaScript. This project 
+                    was made for a friend's college project from the course of Advertising and Marketing.`,
+                    pt: `Este projeto é um timer Pomodoro com um design semelhante ao kindle. O principal objetivo 
+                    é ajudar as pessoas a se concentrarem em seus livros e estudos. O projeto foi feito com HTML, 
+                    CSS e JavaScript. Este projeto foi feito para um projeto da faculdade de um amigo do curso de 
+                    Publicidade e Marketing.`
+                },
+                repository: `${GITHUB_URL}/page-flow`,
+                preview: 'https://page-flow-pi.vercel.app/',
+                languages: ["JavaScript", "HTML", "CSS"]
+            },
+            {
                 name: "API Troco",
                 description: {
                     pt: `Este projeto foi feito durante uma aula, com o intuito de testar os conhecimentos em NodeJs, Express e também com funções recursivas. Ele utiliza
@@ -76,21 +108,24 @@ export default function Page() {
                 languages: ["JavaScript", "NodeJs", "Express", "Swagger"]
             },
             {
-                name: "Salon App API (WIP)",
+                name: "Vai Aonde",
                 description: {
-                    pt: `Este é um projeto em andamento, onde estou desenvolvendo um web app para salões de beleza e barbearias. 
-                    O objetivo desse projeto é ser um projeto white label, em que qualquer cliente possa customizar o app de acordo
-                    com a sua logo, cores e serviços. O projeto tem uma stack completa, com um back-end em NodeJs, um front-end em React
-                    e banco de dados em MongoDB. Atualmente finalizei a parte de funções básicas do back-end, para inserção, atualização, 
-                    leitura e remoção dos dados, irei agora seguir com a implementação de autenticação e autorização.`,
-                    en: `This is a work in progress project, where I'm developing a web app for beauty salons and barbershops.
-                    This project's goal is to be a white label app, where any client can customize the app with their logo, colors palette
-                    and services. The project has a full stack, with a NodeJs back-end, a React front-end and a MongoDB database.
-                    I've finished the basic functions of the back-end, for CRUD operations, and now I'll move on to the implementation
-                    of authentication and authorization.`
+                    pt: `Este é um aplicativo para controle de gastos de uma viagem. O usuário pode adicionar os gastos com gasolina,
+aliemntação, hospedagem e outros. O aplicativo calcula o total gasto. Este aplicativo foi feito para um trabalho na faculdade e desenvolvido em grupo com mais duas pessoas.`,
+                    en: `This is an app for managing travel expenses. The user can add expenses such as gas, food, accommodation, and others. The app calculates the total amount spent. This app was created for a college project and developed in a group with two other people.`
                 },
-                repository: `${GITHUB_URL}/salonApp-api`,
-                languages: ["TypeScript", "Express", "NodeJs", "MongoDB"]
+                repository: `${GITHUB_URL}/AppVaiAonde`,
+                languages: ["Java", "Android Studio"]
+            },
+            {
+                name: "Asterisca Isso",
+                description: {
+                    pt: `Este é um projeto de aplicativo Android que tem como objetivo "traduzir" as frases para que sejam apenas asteriscos. O aplicativo foi desenvolvido em Java com o Android Studio. Esse aplicativo foi desenvolvido em 2021 quando na época, no 
+  Twitter/X, havia uma brincadeira de "traduzir" as frases para que fossem apenas asteriscos. O aplicativo foi desenvolvido para facilitar a brincadeira.`,
+                    en: `This is an Android app project that aims to "translate" phrases so that they are only asterisks. The application was developed in Java with Android Studio. This application was developed in 2021 when at the time, on Twitter/X, there was a joke of "translating" the phrases so that they were only asterisks. The application was developed to facilitate the joke.`
+                },
+                repository: `${GITHUB_URL}/asteriscaIsso`,
+                languages: ["Java", "Android Studio"]
             }
         ])
     }
